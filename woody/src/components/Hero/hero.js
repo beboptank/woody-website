659a696a61cfcfeb58a5da8f19f061e413a4f3ca
beboptank <beboptank12@gmail.com>
2020-01-06 22:05:56 +0900

    Added hero js and scss files, added simple hero component

---
+++ woody/src/components/Hero/hero.js
@@ -0,0 +1,15 @@
+import { Link } from "gatsby";
+import PropTypes from "prop-types";
+import React from "react";
+import "../layout.scss";
+import "hero.scss";
+
+const Hero = () => (
+    <div className="herocontainer">
+        <div className="herocontainer__info">
+            <h1>Bar and Curry in the Heart of Fukui</h1>
+        </div>
+    </div>
+)
+
+export default Hero;
