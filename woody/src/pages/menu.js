e5518f4eaf1ffd80b5b57a3f8fc4bc9be94c58b5
beboptank <beboptank12@gmail.com>
2020-02-17 09:38:20 +0900

    add react-ui-cards package

--- woody/src/pages/menu.js
+++ woody/src/pages/menu.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo";
 const Menu = () => (
     <Layout>
         <SEO title="Menu" />
-        <h1>Hi from the menu</h1>
+        <h1>Menu</h1>
         <p>Welcome to the menu page</p>
         <Link to="/">Go back to the homepage</Link>
     </Layout>
