1f572f442aa2160ffd4b7b775f811501fbf281a9
beboptank <beboptank12@gmail.com>
2020-03-15 21:06:32 +0900

    add icons to contact page

--- woody/src/pages/contact.js
+++ woody/src/pages/contact.js
@@ -1,22 +1,30 @@
 import React from "react";
 import { Link } from "gatsby";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhoneSquare } from "@fortawesome/free-solid-svg-icons";
+import {faFacebookSquare, faInstagramSquare } from "@fortawesome/free-brands-svg-icons";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import Nav from "../components/Nav/nav";
 
 
-import "./scss/menu.scss";
+import "./scss/contact.scss";
 
 
 const Contact = () => (
     <Layout>
         <Nav />
-        <SEO title="woody Menu" />
+        <SEO title="woody Contact" />
         <h1>Contact</h1>
 
         <div className="contactcontainer">
-            
+            <a href="https://www.facebook.com/woodyfukui"><FontAwesomeIcon icon={faFacebookSquare} className="contactcontainer__icon" /></a>
+            <a href="https://www.instagram.com/woody_diary/"><FontAwesomeIcon icon={faInstagramSquare} className="contactcontainer__icon" /></a>
+            <div className="contactcontainer__phone">
+                <a><FontAwesomeIcon className="contactcontainer__icon" icon={faPhoneSquare} /></a>
+                <p className="contactcontainer__phone__number">090-6608-8099</p>
+            </div>
         </div>
 
         <Link to="/">Home</Link>
