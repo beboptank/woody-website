a59d31b617c4e55cfe174361b8d5968a8abd816e
beboptank <beboptank12@gmail.com>
2020-03-19 23:22:39 +0900

    eliminate x-axis overflow

--- woody/src/pages/menu.js
+++ woody/src/pages/menu.js
@@ -13,58 +13,60 @@ const Menu = () => (
     <Layout>
         <Nav />
         <SEO title="woody Menu" />
-        <h1>Menu</h1>
+        <div className="menucontainer">
+            <h1>Menu</h1>
 
-        <div>
-            <MenuCard 
-                type="Beer"
-                itemlist={[
-                    "Budweiser ¥650", 
-                    "Corona ¥700",
-                    "Heinekien ¥700",
-                    "Guinness ¥700",
-                    "Heartland ¥700",
-                    "Hoegarden ¥700",
-                    "Sapporo ¥700",
-                    "Kirin ¥700",
-                    "Shandy Gaff ¥700",
-                    "Campari Beer ¥700",
-                    "Tokyo White ¥750",
-                    "Brooklyn Lager ¥750",
-                    "Blue Moon ¥750",
-                    "Goose Island IPA ¥750"
+            <div>
+                <MenuCard 
+                    type="Beer"
+                    itemlist={[
+                        "Budweiser ¥650", 
+                        "Corona ¥700",
+                        "Heinekien ¥700",
+                        "Guinness ¥700",
+                        "Heartland ¥700",
+                        "Hoegarden ¥700",
+                        "Sapporo ¥700",
+                        "Kirin ¥700",
+                        "Shandy Gaff ¥700",
+                        "Campari Beer ¥700",
+                        "Tokyo White ¥750",
+                        "Brooklyn Lager ¥750",
+                        "Blue Moon ¥750",
+                        "Goose Island IPA ¥750"
+                        ]}
+                />
+                <MenuCard 
+                    type="Cocktails"
+                    itemlist={[
+                        "Rum and Coke ¥700",
+                        "Mai-Tai ¥700",
+                        "Sex on the Beach ¥700",
+                        "Sea Breeze ¥700",
+                        "Campari Orange ¥700",
+                        "Campari Soda ¥700",
+                        "Screwdriver ¥700",
+                        "Salty Dog ¥700",
+                        "Moscow Mule ¥750",
+                        "Sakura Ginger ¥750",
+                        "Gin & Tonic ¥750",
+                        "Jägerbomb ¥800",
+                        "Cocabomb ¥800",
+                        "XYZ ¥800",
+                        "Piña Colada ¥800"
                     ]}
-            />
-            <MenuCard 
-                type="Cocktails"
-                itemlist={[
-                    "Rum and Coke ¥700",
-                    "Mai-Tai ¥700",
-                    "Sex on the Beach ¥700",
-                    "Sea Breeze ¥700",
-                    "Campari Orange ¥700",
-                    "Campari Soda ¥700",
-                    "Screwdriver ¥700",
-                    "Salty Dog ¥700",
-                    "Moscow Mule ¥750",
-                    "Sakura Ginger ¥750",
-                    "Gin & Tonic ¥750",
-                    "Jägerbomb ¥800",
-                    "Cocabomb ¥800",
-                    "XYZ ¥800",
-                    "Piña Colada ¥800"
-                ]}
-            />
-            <MenuCard 
-                type="Food"
-                itemlist={[
-                    "Curry & Rice ¥1000",
-                    "French Fries ¥800"
-                ]}
-            />
+                />
+                <MenuCard 
+                    type="Food"
+                    itemlist={[
+                        "Curry & Rice ¥1000",
+                        "French Fries ¥800"
+                    ]}
+                />
+            </div>
+            
+            <Link to="/">Home</Link>
         </div>
-        
-        <Link to="/">Home</Link>
     </Layout>
 )
 
