1b951ff97c48859eb88103187d07b047db81e0e4
beboptank <beboptank12@gmail.com>
2020-04-01 23:35:44 +0900

    initial code for Google Calendar API call

---
+++ woody/src/components/Calendar/calendar.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import {Link} from 'gatsby';
+
+
+let googleCalendarUrl = calId =>
+    `https://www.googleapis.com/calendar/v3/calendars/${calId}/events`
+    + `?singleEvents=true&key=${API_KEY}`;
+const API_KEY = 'AIzaSyCVG9LqW7EdCVepSCoFIh3umavMHP3QwP0';
+
+class Calendar extends Component {
+    constructor() {
+        super();
+        this.state = {
+            events: []
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://www.googleapis.com/auth/calendar.events.readonly')
+        .then(response => {
+            return response.json();
+        })
+        .then(events => {
+            console.log(events);
+        });
+    }
+}
